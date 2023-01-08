b2bbd18c13a809279a366a0bae00b9c0f1306817
Dua Lipa <quentin.vedrenne74@gmail.com>
2023-01-08 02:21:03 +0100

    add config

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+export default {
+	apiGateway: {
+		REGION: 'eu-west-3',
+		URL: 'https://8d9r6mzn0j.execute-api.eu-west-3.amazonaws.com/Stage/'
+	},
+	cognito: {
+		REGION: 'eu-west-3',
+		USER_POOL_ID: 'eu-west-3_uGAFHQkLs',
+		APP_CLIENT_ID: '726drblr85sqsjg7u1f452ohvh',
+		DOMAIN: 'epitech-openid.auth.eu-west-3.amazoncognito.com',
+		SCOPE: ['email', 'openid'],
+		REDIRECT_SIGN_IN: 'http://localhost:5173/callback/',
+		REDIRECT_SIGN_OUT: 'http://localhost:5173/',
+		RESPONSE_TYPE: 'code'
+	}
+};
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://d2j6wz6ccd9hih.cloudfront.net",
+  base: "https://d1sfectf2q28h4.cloudfront.net",
   plugins: [react()],
   resolve: {
     alias: {
